1d2ebd703cd0a348b71c6c52130150253b8ac540
Abdul <abdul_h20@mhs.unsyiah.ac.id>
2023-10-06 11:59:19 +0700

    fix: fixing a listofigure and listtab

---
+++ build.sh
@@ -0,0 +1,16 @@
+pdflatex -interaction=nonstopmode --shell-escape seminar-proposal.tex;
+bibtex seminar-proposal.aux ;
+pdflatex -interaction=nonstopmode --shell-escape seminar-proposal.tex;
+pdflatex -interaction=nonstopmode --shell-escape seminar-proposal.tex;
+
+# Uncommnet bagian ini jika inigin mengkompile file seminar-hasil
+#pdflatex -interaction=nonstopmode --shell-escape seminar-hasil.tex;
+#bibtex seminar-hasil.aux ;
+#pdflatex -interaction=nonstopmode --shell-escape seminar-hasil.tex;
+#pdflatex -interaction=nonstopmode --shell-escape seminar-hasil.tex;
+
+# Uncomment bagian ini jika ingin mencompile file skripsi
+#pdflatex -interaction=nonstopmode --shell-escape skripsi.tex;
+#bibtex skripsi.aux ;
+#pdflatex -interaction=nonstopmode --shell-escape skripsi.tex;
+#pdflatex -interaction=nonstopmode --shell-escape skripsi.tex;
